a9fa226b12f1196de5143b6e191d59fb9451b244
sarahwonago <sarahshaaleh@gmail.com>
2024-11-02 18:54:14 +0300

    lease model

--- lease/models.py
+++ lease/models.py
@@ -1,3 +1,39 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from tenant.models import Tenant
+
+class Lease(models.Model):
+    """
+    Model representing a lease.
+    """
+
+    id = models.UUIDField(
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4
+    )
+
+    tenant = models.OneToOneField(
+        Tenant, 
+        related_name="lease",
+        on_delete=models.CASCADE
+        )
+    
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    account = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00
+    )
+
+    def __str__(self):
+        return f"Lease:{self.tenant}"
+    
+    
+
+
+
